c735c59ff9e640e514da4aeb95a96211bdb16a2e
bronnt <bthurber@brian-t.umc.utah.edu>
2016-05-23 13:09:54 -0600

    formatting

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>HTML5 boilerplate – all you really need…</title>
-	<link rel="stylesheet" href="css/style.css">
-	<!--[if IE]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>Whitecap Interactive</title>
+		<link rel="stylesheet" href="css/style.css">
+		<!--[if IE]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body id="home">
 
-<body id="home">
+		<h1>Welcome to Whitecap Interactive</h1>
 
-	<h1>HTML5 boilerplate</h1>
-
-</body>
+	</body>
 <script src="js/jquery-2.2.3.min.js"></script>	
 <script src="js/whitecap.js"></script>
 </html>
