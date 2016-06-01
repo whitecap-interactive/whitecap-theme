741ae394c4121310cd9a07e14176773349a927f2
bronnt <bthurber@brian-t.ddi.utah.edu>
2016-06-01 15:39:36 -0600

    stuff

--- index.php
+++ index.php
@@ -12,6 +12,23 @@
 		<div class="bg">
 			<h1>Welcome to Whitecap Interactive This shit is gonna be Boss!!</h1>
 		</div>
+		<header id="masthead" class="site-header" role="banner">
+			<div class="site-branding">
+				<a href="http://whitecap.io:8888/" rel="home"><div class="logo"></div></a>
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">Primary Menu</button>
+				<div id="primary-menu" class="menu">
+					<ul aria-expanded="false" class=" nav-menu">
+						<li class="page_item"><a href="http://blackcap.io:8888/about-us/">About Us</a></li>
+						<li class="page_item"><a href="http://blackcap.io:8888/contact/">Contact</a></li>
+						<li class="page_item"><a href="http://blackcap.io:8888/our-work/">Our Work</a></li>
+						<li class="page_item current_page_item"><a href="http://blackcap.io:8888/">Services</a></li>
+					</ul>
+				</div>
+			</nav><!-- #site-navigation -->
+		</header>
 	</body>
 <script src="js/jquery-2.2.3.min.js"></script>	
 <script src="js/whitecap.js"></script>
