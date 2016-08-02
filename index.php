3463515a213dcff411d3efde2486f8fc17cb5c1e
Dave White <david@dvdwhite.com>
2016-08-02 12:41:08 -0600

    added temp directory

--- index.php
+++ index.php
@@ -14,17 +14,17 @@
 		</div>
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
-				<a href="http://whitecap.io:8888/" rel="home"><div class="logo"></div></a>
+				<a href="/" rel="home"><div class="logo"></div></a>
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">Primary Menu</button>
 				<div id="primary-menu" class="menu">
 					<ul aria-expanded="false" class=" nav-menu">
-						<li class="page_item"><a href="http://blackcap.io:8888/about-us/">About Us</a></li>
-						<li class="page_item"><a href="http://blackcap.io:8888/contact/">Contact</a></li>
-						<li class="page_item"><a href="http://blackcap.io:8888/our-work/">Our Work</a></li>
-						<li class="page_item current_page_item"><a href="http://blackcap.io:8888/">Services</a></li>
+						<li class="page_item"><a href="#">About Us</a></li>
+						<li class="page_item"><a href="#">Contact</a></li>
+						<li class="page_item"><a href="#">Our Work</a></li>
+						<li class="page_item current_page_item"><a href="#">Services</a></li>
 					</ul>
 				</div>
 			</nav><!-- #site-navigation -->
